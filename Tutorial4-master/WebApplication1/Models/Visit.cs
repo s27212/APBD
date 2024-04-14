0cfd0619859956389e19d873469f434b7f6fdb74
s27212 <s27212@pjwstk.edu.pl>
2024-04-14 23:52:50 +0200

    Task 4 added

--- Tutorial4-master/WebApplication1/Models/Visit.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication1.Models;
-
-public class Visit
-{
-    public DateTime DateTime { get; set; }
-    public int AnimalId { get; set; }
-    public string Description { get; set; }
-    public double Price { get; set; }
-}
